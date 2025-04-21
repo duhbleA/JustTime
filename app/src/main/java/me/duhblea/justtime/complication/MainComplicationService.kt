b1429bc6015ea1e98d1c4c983d2ffd49b09a1956
Austin Alderton <austin.d.alderton@tactware.net>
2025-04-21 09:39:34 -0500

    Simplify approach to calculating progress using seconds in a day. Properly define update period and supported complication types. Support goal progress complication data type.

--- app/src/main/java/me/duhblea/justtime/complication/MainComplicationService.kt
+++ app/src/main/java/me/duhblea/justtime/complication/MainComplicationService.kt
@@ -7,6 +7,7 @@ import android.provider.AlarmClock
 import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationText
 import androidx.wear.watchface.complications.data.ComplicationType
+import androidx.wear.watchface.complications.data.GoalProgressComplicationData
 import androidx.wear.watchface.complications.data.LongTextComplicationData
 import androidx.wear.watchface.complications.data.MonochromaticImage
 import androidx.wear.watchface.complications.data.PlainComplicationText
@@ -17,21 +18,15 @@ import androidx.wear.watchface.complications.datasource.ComplicationRequest
 import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
 import me.duhblea.justtime.R
 import java.time.LocalDateTime
+import java.time.temporal.ChronoField
 
 /**
- * Skeleton for complication data source that returns short text.
+ * Serves the complication.
  */
 class MainComplicationService : SuspendingComplicationDataSourceService() {
-    private val timeFormat = "h:mm"
-
-    private fun launchIntent(): PendingIntent? {
-
-        val alarmClockIntent = Intent(AlarmClock.ACTION_SHOW_ALARMS)
-
-        return PendingIntent.getActivity(
-            this, 0, alarmClockIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
+    companion object {
+        private const val TIME_FORMAT = "h:mm"
+        private const val SECONDS_IN_DAY = 86400f
     }
 
     override fun getPreviewData(type: ComplicationType): ComplicationData? {
@@ -70,11 +65,29 @@ class MainComplicationService : SuspendingComplicationDataSourceService() {
                     .build()
             }
 
+            ComplicationType.GOAL_PROGRESS -> {
+                GoalProgressComplicationData.Builder(
+                    value = 608f,
+                    targetValue = SECONDS_IN_DAY,
+                    contentDescription = ComplicationText.EMPTY,
+                )
+                    .setTitle(null)
+                    .setText(PlainComplicationText.Builder(text = "10:08").build())
+                    .setMonochromaticImage(
+                        MonochromaticImage.Builder(
+                            image = Icon.createWithResource(
+                                this,
+                                R.drawable.ic_clock
+                            )
+                        ).build()
+                    ).build()
+            }
+
             ComplicationType.RANGED_VALUE -> {
                 RangedValueComplicationData.Builder(
                     value = 608f,
                     min = 0f,
-                    max = 1440f,
+                    max = SECONDS_IN_DAY,
                     contentDescription = ComplicationText.EMPTY
                 )
                     .setText(PlainComplicationText.Builder(text = "10:08").build())
@@ -97,19 +110,15 @@ class MainComplicationService : SuspendingComplicationDataSourceService() {
     }
 
     override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData {
-
         val currentTime = LocalDateTime.now()
-        val hour = currentTime.hour
-        val min = currentTime.minute
+        val progress = currentTime.get(ChronoField.SECOND_OF_DAY).toFloat()
 
-        val progress = hour * 60 + min.toFloat()
-
-        val text = TimeFormatComplicationText.Builder(format = timeFormat).build()
+        val currentTimeText = TimeFormatComplicationText.Builder(format = TIME_FORMAT).build()
 
         return when (request.complicationType) {
             ComplicationType.SHORT_TEXT -> {
                 ShortTextComplicationData.Builder(
-                    text = text,
+                    text = currentTimeText,
                     contentDescription = ComplicationText.EMPTY
                 )
                     .setTitle(null)
@@ -121,13 +130,13 @@ class MainComplicationService : SuspendingComplicationDataSourceService() {
                             )
                         ).build()
                     )
-                    .setTapAction(launchIntent())
+                    .setTapAction(createLaunchIntent())
                     .build()
             }
 
             ComplicationType.LONG_TEXT -> {
                 LongTextComplicationData.Builder(
-                    text = text,
+                    text = currentTimeText,
                     contentDescription = ComplicationText.EMPTY
                 )
                     .setTitle(null)
@@ -139,7 +148,7 @@ class MainComplicationService : SuspendingComplicationDataSourceService() {
                             )
                         ).build()
                     )
-                    .setTapAction(launchIntent())
+                    .setTapAction(createLaunchIntent())
                     .build()
             }
 
@@ -147,10 +156,10 @@ class MainComplicationService : SuspendingComplicationDataSourceService() {
                 RangedValueComplicationData.Builder(
                     value = progress,
                     min = 0f,
-                    max = 1440f,
+                    max = SECONDS_IN_DAY,
                     contentDescription = ComplicationText.EMPTY
                 )
-                    .setText(text)
+                    .setText(currentTimeText)
                     .setTitle(null)
                     .setMonochromaticImage(
                         MonochromaticImage.Builder(
@@ -160,13 +169,41 @@ class MainComplicationService : SuspendingComplicationDataSourceService() {
                             )
                         ).build()
                     )
-                    .setTapAction(launchIntent())
+                    .setTapAction(createLaunchIntent())
                     .build()
+            }
+
+            ComplicationType.GOAL_PROGRESS -> {
+                GoalProgressComplicationData.Builder(
+                    value = progress,
+                    targetValue = SECONDS_IN_DAY,
+                    contentDescription = ComplicationText.EMPTY,
+                )
+                    .setTitle(null)
+                    .setText(currentTimeText)
+                    .setTapAction(createLaunchIntent())
+                    .setMonochromaticImage(
+                        MonochromaticImage.Builder(
+                            image = Icon.createWithResource(
+                                this,
+                                R.drawable.ic_clock
+                            )
+                        ).build()
+                    ).build()
             }
 
             else -> {
                 throw IllegalStateException("Unexpected complication type")
             }
         }
+    }
+
+    private fun createLaunchIntent(): PendingIntent? {
+        val alarmClockIntent = Intent(AlarmClock.ACTION_SHOW_ALARMS)
+
+        return PendingIntent.getActivity(
+            this, 0, alarmClockIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
     }
 }
